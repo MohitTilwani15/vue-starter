5377ee31c4e6298e9ee068bada12f355cca1ae3b
Mohit Tilwani <mtilwani@deloitte.de>
2019-04-22 17:43:22 +0200

    feat(scss & webpack): whitelabel solution
    add a whitelabel solution, to specify different types of themes(brand)
    create a theme folder containing _variables.scss file and add append new theme in whitelabel.config.js
    
    Thsi would close the issue https://github.com/vuesion/vuesion/issues/391

---
+++ .vuesion/whitelabel.config.js
@@ -0,0 +1,12 @@
+const whitelabelConfig = {
+  default: {
+    brand: 'Default Theme',
+    scssVariables: 'src/app/shared/designSystem/variables/theme/default',
+  },
+  dark: {
+    brand: 'Dark Theme',
+    scssVariables: 'src/app/shared/designSystem/variables/theme/dark',
+  },
+};
+
+module.exports = whitelabelConfig;
